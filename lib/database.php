1bbd9d9607959ca51d60b84dd69026762fc7d530
dev-pop <dev@dev.com>
2022-12-24 23:52:33 +0700

    update

--- lib/database.php
+++ lib/database.php
@@ -1,32 +1,45 @@
 <?php
-class Database{
-    private static $database_name ='users';
+class Database
+{
     private static $database_host = 'localhost';
+    private static $database_name = 'test';
     private static $database_user = 'root';
-    private static $database_user_password = 'root';
-
+    private static $database_user_password = '';
     private static $connection_status = null;
 
-    public function __construct(){
+    /**
+     * Summary of __construct
+     */
+    public function __construct()
+    {
         die('Init function is not allowed');
     }
 
+    /**
+     * Summary of connect
+     * @return PDO|null
+     */
     public static function connect()
     {
-        if(self::$connection_status == null)
-        try{
-           self::$connection_status = new PDO('mysql:host='.self::$database_host.';dbname='.self::$database_name.'', self::$database_user, self::$database_user_password);
-
-        }catch(PDOException $e)
-        {
-            die($e->getMessage());
-        }
+        if (self::$connection_status == null)
+            try {
+                self::$connection_status = new PDO(
+                    'mysql:host=' . self::$database_host . ';dbname=' . self::$database_name . '',
+                    self::$database_user,
+                    self::$database_user_password
+                );
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
         return self::$connection_status;
     }
+
+    /**
+     * Summary of disconnect
+     * @return void
+     */
     public static function disconnect()
     {
         self::$connection_status = null;
     }
 }
-
-?>
--- lib/functions.php
+++ lib/functions.php
@@ -1,129 +1,122 @@
 <?php
 include 'database.php';
 
+/**
+ * Summary of get_all_user_list
+ * @return array
+ */
 function get_all_user_list()
 {
     $pdo = Database::connect();
     $sql = "SELECT * FROM user";
-
     try {
-
         $query = $pdo->prepare($sql);
         $query->execute();
         $all_user_info = $query->fetchAll(PDO::FETCH_ASSOC);
-
     } catch (PDOException $e) {
-
         print "Error!: " . $e->getMessage() . "<br/>";
         die();
     }
-
     Database::disconnect();
     return $all_user_info;
 }
 
+/**
+ * Summary of get_single_user_info
+ * @param mixed $id
+ * @return array
+ */
 function get_single_user_info($id)
 {
     $pdo = Database::connect();
     $sql = "SELECT * FROM user where id = {$id} ";
-
     try {
-
         $query = $pdo->prepare($sql);
         $query->execute();
         $user_info = $query->fetchAll(PDO::FETCH_ASSOC);
-
     } catch (PDOException $e) {
-
         print "Error!: " . $e->getMessage() . "<br/>";
         die();
     }
-
     Database::disconnect();
     return $user_info;
 }
 
-
-
-
-function update_user_info($id,$name,$email)
+/**
+ * Summary of update_user_info
+ * @param mixed $id
+ * @param mixed $name
+ * @param mixed $email
+ * @return array<string>
+ */
+function update_user_info($id, $name, $email)
 {
     $pdo = Database::connect();
     $sql = "UPDATE user SET name = '{$name}', email = '{$email}' where id = '{$id}'";
     $status = [];
-
     try {
-
         $query = $pdo->prepare($sql);
         $result = $query->execute();
-        if($result)
-        {
+        if ($result) {
             $status['message'] = "Data updated";
-        }
-        else{
+        } else {
             $status['message'] = "Data is not updated";
         }
-
     } catch (PDOException $e) {
-
-        $status['message'] = $e->getMessage(); 
+        $status['message'] = $e->getMessage();
     }
-
     Database::disconnect();
     return $status;
 }
 
-
-function add_user_info($name,$email)
+/**
+ * Summary of add_user_info
+ * @param mixed $name
+ * @param mixed $email
+ * @return array<string>
+ */
+function add_user_info($name, $email)
 {
     $pdo = Database::connect();
     $sql = "INSERT INTO user(`name`,`email`) VALUES('{$name}', '{$email}')";
     $status = [];
-
     try {
-
         $query = $pdo->prepare($sql);
         $result = $query->execute();
-        if($result)
-        {
+        if ($result) {
             $status['message'] = "Data inserted";
-        }
-        else{
+        } else {
             $status['message'] = "Data is not inserted";
         }
-
     } catch (PDOException $e) {
-
-        $status['message'] = $e->getMessage(); 
+        $status['message'] = $e->getMessage();
     }
 
     Database::disconnect();
     return $status;
 }
 
+/**
+ * Summary of delete_user_info
+ * @param mixed $id
+ * @return array<string>
+ */
 function delete_user_info($id)
 {
     $pdo = Database::connect();
-    $sql ="DELETE FROM user where id = '{$id}'";
+    $sql = "DELETE FROM user where id = '{$id}'";
     $status = [];
-
     try {
-
         $query = $pdo->prepare($sql);
         $result = $query->execute();
-        if($result)
-        {
+        if ($result) {
             $status['message'] = "Data deleted";
-        }
-        else{
+        } else {
             $status['message'] = "Data is not deleted";
         }
-
     } catch (PDOException $e) {
-
-        $status['message'] = $e->getMessage(); 
+        $status['message'] = $e->getMessage();
     }
-
     Database::disconnect();
     return $status;
 }
